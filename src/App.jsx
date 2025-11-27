b93c594e17738992fd4cee12d6162b5edbcf9c23
Kaarshnav <rajneeshupreti98@gmail.com>
2025-11-28 00:55:02 +0530

    ☕️ added fancy pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import { Outlet } from "react-router";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Heelo from sip N snack</h1>
       <Header />
-      <NavBar />
+      {/* <NavBar /> */}
       <Outlet />
       <Footer />
     </>
